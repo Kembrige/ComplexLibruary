200db292c2e0a5b664af68c5b39724ee4bf251fe
Kembrige <49879155+Kembrige@users.noreply.github.com>
2024-03-31 19:50:09 +0400

    Rework class

--- ComplexLibruary/ComplexNumber.cs
+++ ComplexLibruary/ComplexNumber.cs
@@ -1,57 +1,59 @@
 ﻿using System;
 
-namespace ComplexLibruary
+public class RationalNumber
 {
-    public class ComplexNumber
+    public int Numerator { get; }
+    public int Denominator { get; }
+
+    public RationalNumber(int numerator, int denominator)
     {
-        
-        private int real;
-        private int imaginary;
+        int gcd = GCD(numerator, denominator);
+        Numerator = numerator / gcd;
+        Denominator = denominator / gcd;
+    }
 
-        public ComplexNumber()
+    private int GCD(int a, int b)
+    {
+        return b == 0 ? a : GCD(b, a % b);
+    }
 
-        {
-            Console.WriteLine("Введите числитель:");
-            real = Convert.ToInt32(Console.ReadLine());
+    public static RationalNumber Add(RationalNumber r1, RationalNumber r2)
+    {
+        int numerator = r1.Numerator * r2.Denominator + r2.Numerator * r1.Denominator;
+        int denominator = r1.Denominator * r2.Denominator;
+        return new RationalNumber(numerator, denominator);
+    }
 
-            Console.WriteLine("Введите знаменатель:");
-            imaginary = Convert.ToInt32(Console.ReadLine());
-        }
-        public ComplexNumber(int real, int imaginary)
-        {
-            this.real = real;
-            this.imaginary = imaginary;
-        }
+    public static RationalNumber Multiply(RationalNumber r1, RationalNumber r2)
+    {
+        int numerator = r1.Numerator * r2.Numerator;
+        int denominator = r1.Denominator * r2.Denominator;
+        return new RationalNumber(numerator, denominator);
+    }
 
-        public ComplexNumber Add(ComplexNumber other)
-        {
-            return new ComplexNumber(this.real + other.real, this.imaginary + other.imaginary);
-        }
-
-        public ComplexNumber Multiply(ComplexNumber other)
-        {
-            int newReal = this.real * other.real - this.imaginary * other.imaginary;
-            int newImaginary = this.real * other.imaginary + this.imaginary * other.real;
-            return new ComplexNumber(newReal, newImaginary);
-        }
-
-        public ComplexNumber Divide(ComplexNumber other)
-        {
-            int denominator = other.real * other.real + other.imaginary * other.imaginary;
-            int newReal = (this.real * other.real + this.imaginary * other.imaginary) / denominator;
-            int newImaginary = (this.imaginary * other.real - this.real * other.imaginary) / denominator;
-            return new ComplexNumber(newReal, newImaginary);
-        }
-
-        public ComplexNumber MultiplyByRational(int rational)
-        {
-            return new ComplexNumber(this.real * rational, this.imaginary * rational);
-        }
-
-        public override string ToString()
-        {
-            return $"{real} + {imaginary}i";
-        }
+    public static RationalNumber Divide(RationalNumber r1, RationalNumber r2)
+    {
+        int numerator = r1.Numerator * r2.Denominator;
+        int denominator = r1.Denominator * r2.Numerator;
+        return new RationalNumber(numerator, denominator);
     }
 }
 
+public class ComplexNumber
+{
+    public double Real { get; }
+    public double Imaginary { get; }
+
+    public ComplexNumber(double real, double imaginary)
+    {
+        Real = real;
+        Imaginary = imaginary;
+    }
+
+    public static ComplexNumber MultiplyByRational(ComplexNumber c, RationalNumber r)
+    {
+        double real = c.Real * r.Numerator;
+        double imaginary = c.Imaginary * r.Numerator;
+        return new ComplexNumber(real, imaginary);
+    }
+}
